92442c49264e02c2723571b8d06a21a9a74eadb2
Berenjeneitor444 <dciriano2@gmail.com>
2025-01-09 16:42:07 +0100

    SplashScreen

---
+++ app/src/main/java/com/example/splash2/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.splash2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity.this, MenuScreenActivity.class));
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller = new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+        logo1.startAnimation(fade1);
+        logo2.startAnimation(fade2);
+
+        }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        findViewById(R.id.TextViewTopTitle).clearAnimation();
+        findViewById(R.id.TextViewBottomTitle).clearAnimation();
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+    }
+}
