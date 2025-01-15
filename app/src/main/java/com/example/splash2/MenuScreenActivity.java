85d016a36a86cc9c3e264cd1eecbb584705359f5
Berenjeneitor444 <dciriano2@gmail.com>
2025-01-15 17:29:13 +0100

    menu finished

--- app/src/main/java/com/example/splash2/MenuScreenActivity.java
+++ app/src/main/java/com/example/splash2/MenuScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.splash2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,12 @@ public class MenuScreenActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView menuList = (ListView) findViewById(R.id.list);
+        String[] items = {getResources().getString(R.string.menu_item_play),
+                getResources().getString(R.string.menu_item_scores),
+                getResources().getString(R.string.menu_item_settings),
+                getResources().getString(R.string.menu_item_help)};
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, R.layout.menu_item, items);
+        menuList.setAdapter(adapt);
     }
 }
